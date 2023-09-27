173d2c55f4bd11f9b9e6c8c4a0e814e6e8547305
Luancss <ventobo123@gmail.com>
2023-09-27 16:46:22 -0300

    Criando hook de listagem

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,6 @@
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
-import { useQuery } from "react-query";
+
 import {
   Box,
   Button,
@@ -21,34 +21,11 @@ import {
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import Link from "next/link";
-import { useEffect } from "react";
-import { api } from "@/src/services/api";
+
+import { useUsers } from "@/src/services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery(
-    "users",
-    async () => {
-      const {data} = await api.get("users");
-
-      const users = data.users.map((user) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-            day: "2-digit",
-            month: "long",
-            year: "numeric",
-          }),
-        };
-      });
-      return users;
-    },
-    {
-      staleTime: 1000 * 5,
-    }
-  );
-
+  const { data, isLoading, isFetching, error } = useUsers();
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
